ca51a73b2686474c11ae8bf20d7eb0bd3a8eeb46
miha <vadim.hramov.69@mail.ru>
2018-12-28 22:47:27 +0200

    create method "getNewsListComponent"

--- app/src/main/java/aaa/bbb/ccc/sportnews/NewsApp.java
+++ app/src/main/java/aaa/bbb/ccc/sportnews/NewsApp.java
@@ -2,6 +2,23 @@ package aaa.bbb.ccc.sportnews;
 
 import android.app.Application;
 
+
+import aaa.bbb.ccc.sportnews.di.component.DaggerNewsAppComponent;
+import aaa.bbb.ccc.sportnews.di.component.NewsAppComponent;
+import aaa.bbb.ccc.sportnews.di.component.NewsListComponent;
+import aaa.bbb.ccc.sportnews.di.module.ApplicationModule;
+
 public class NewsApp extends Application {
 
+    static NewsAppComponent component;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        component = DaggerNewsAppComponent.builder().applicationModule(new ApplicationModule(this)).build();
+    }
+
+    public static NewsListComponent getNewsListComponent() {
+        return component.getNewsListComponent();
+    }
 }
