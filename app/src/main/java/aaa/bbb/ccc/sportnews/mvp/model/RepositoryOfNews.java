b15d70fa6324d3ab8d9d500dc17d270e5daf37cd
miha <vadim.hramov.69@mail.ru>
2018-12-28 22:25:18 +0200

    fix "javax.net.ssl.sslhandshakeexception java.security.cert.certpathvalidatorexception"

--- app/src/main/java/aaa/bbb/ccc/sportnews/mvp/model/RepositoryOfNews.java
+++ app/src/main/java/aaa/bbb/ccc/sportnews/mvp/model/RepositoryOfNews.java
@@ -1,8 +1,12 @@
 package aaa.bbb.ccc.sportnews.mvp.model;
 
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import aaa.bbb.ccc.sportnews.api.NyNewsApi;
 import aaa.bbb.ccc.sportnews.pojo.News;
@@ -17,15 +21,59 @@ import rx.Observable;
 public class RepositoryOfNews implements IRepositoryOfNews{
     private NyNewsApi api;
 
+    private OkHttpClient getUnsafeOkHttpClient() {
+
+        try {
+            final TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(
+                        java.security.cert.X509Certificate[] chain,
+                        String authType) {
+                }
+
+                @Override
+                public void checkServerTrusted(
+                        java.security.cert.X509Certificate[] chain,
+                        String authType) {
+                }
+
+                @Override
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return new java.security.cert.X509Certificate[0];
+                }
+            } };
+
+            // Install the all-trusting trust manager
+            final SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, trustAllCerts,
+                    new java.security.SecureRandom());
+            // Create an ssl socket factory with our all-trusting manager
+            final SSLSocketFactory sslSocketFactory = sslContext
+                    .getSocketFactory();
+
+            X509TrustManager trustManager = (X509TrustManager) trustAllCerts[0];//
+            OkHttpClient okHttpClient = new OkHttpClient();
+            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+            okHttpClient = okHttpClient.newBuilder()
+                    .sslSocketFactory(sslSocketFactory,trustManager)
+                    .addInterceptor(interceptor)
+                    .hostnameVerifier(org.apache.http.conn.ssl.SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER).build();
+
+            return okHttpClient;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
     public RepositoryOfNews() {
-        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
         Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
-        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
         Retrofit retrofit = new Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .baseUrl(NyNewsApi.BASE_URL)
-                .client(client).build();//базовый url
+                .client(getUnsafeOkHttpClient()).build();//базовый url
         api = retrofit.create(NyNewsApi.class);
     }
 
