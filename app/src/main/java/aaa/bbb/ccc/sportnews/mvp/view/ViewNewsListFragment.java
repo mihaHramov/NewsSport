ee5ed6be025abf282b16ff6505378e4e95fa73b4
miha <vadim.hramov.69@mail.ru>
2018-12-29 22:45:06 +0200

    update StrategyType

--- app/src/main/java/aaa/bbb/ccc/sportnews/mvp/view/ViewNewsListFragment.java
+++ app/src/main/java/aaa/bbb/ccc/sportnews/mvp/view/ViewNewsListFragment.java
@@ -1,11 +1,13 @@
 package aaa.bbb.ccc.sportnews.mvp.view;
 
 import com.arellomobile.mvp.MvpView;
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 
 import java.util.List;
 
 import aaa.bbb.ccc.sportnews.pojo.Article;
-
+@StateStrategyType(value = AddToEndSingleStrategy.class)
 public interface ViewNewsListFragment extends MvpView {
     void showNews(List<Article> news);
     void loading(Boolean b);
