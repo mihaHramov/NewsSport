3b087cfd5a869f3ad6e318ce2e2ecd083b6663c8
miha <vadim.hramov.69@mail.ru>
2018-12-29 22:10:21 +0200

    use strategy type "moxy"

--- app/src/main/java/aaa/bbb/ccc/sportnews/mvp/view/ViewNewsActivity.java
+++ app/src/main/java/aaa/bbb/ccc/sportnews/mvp/view/ViewNewsActivity.java
@@ -2,14 +2,21 @@ package aaa.bbb.ccc.sportnews.mvp.view;
 
 
 import com.arellomobile.mvp.MvpView;
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
+import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy;
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 
 import java.util.List;
 
 import aaa.bbb.ccc.sportnews.mvp.model.Source;
 
 
-public interface ViewNewsActivity  extends MvpView{
+public interface ViewNewsActivity extends MvpView {
     void showMenu(List<Source> globalSources);
+
+    @StateStrategyType(value = AddToEndSingleStrategy.class)
     void selectItemMenu(Integer id);
+
+    @StateStrategyType(value = OneExecutionStateStrategy.class)
     void showNews(Source string);
 }
