5030fe4ece822a2a3533fc8806a4be1d7deb8be8
miha <vadim.hramov.69@mail.ru>
2018-12-29 21:56:51 +0200

    update Source

--- app/src/main/java/aaa/bbb/ccc/sportnews/mvp/model/Source.java
+++ app/src/main/java/aaa/bbb/ccc/sportnews/mvp/model/Source.java
@@ -2,10 +2,19 @@ package aaa.bbb.ccc.sportnews.mvp.model;
 
 public class Source {
     private String name;
+    private String url;
     private Integer id;
 
     public String getName() {
         return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
     }
 
     public void setName(String name) {
