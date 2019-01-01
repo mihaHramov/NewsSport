9d00678796568e0ad7cdad4a480c33b41fb4fc66
miha <vadim.hramov.69@mail.ru>
2019-01-01 23:31:11 +0200

    create new  presenter "PresenterDetailsNews"

---
+++ app/src/main/java/aaa/bbb/ccc/sportnews/mvp/presenter/PresenterDetailsNews.java
@@ -0,0 +1,20 @@
+package aaa.bbb.ccc.sportnews.mvp.presenter;
+
+import com.arellomobile.mvp.InjectViewState;
+import com.arellomobile.mvp.MvpPresenter;
+
+import aaa.bbb.ccc.sportnews.mvp.model.pojo.Article;
+import aaa.bbb.ccc.sportnews.mvp.view.ViewDetailsNews;
+
+@InjectViewState
+public class PresenterDetailsNews extends MvpPresenter<ViewDetailsNews> {
+    private Article article;
+
+    public PresenterDetailsNews(Article article) {
+        this.article = article;
+        getViewState().initView(article);
+    }
+    public void clickShowNewsInSource(){
+        getViewState().showInSource(article);
+    }
+}
