8e4685c8eeff5bd0f774ff09f51d70481a4488ca
miha <vadim.hramov.69@mail.ru>
2019-01-01 23:29:57 +0200

    update "ViewDetailsNews"

--- app/src/main/java/aaa/bbb/ccc/sportnews/mvp/view/ViewDetailsNews.java
+++ app/src/main/java/aaa/bbb/ccc/sportnews/mvp/view/ViewDetailsNews.java
@@ -1,9 +1,13 @@
 package aaa.bbb.ccc.sportnews.mvp.view;
 
 import com.arellomobile.mvp.MvpView;
+import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy;
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 
 import aaa.bbb.ccc.sportnews.mvp.model.pojo.Article;
 
 public interface ViewDetailsNews extends MvpView {
     void initView(Article article);
+    @StateStrategyType(value = OneExecutionStateStrategy.class)
+    void showInSource(Article article);
 }
