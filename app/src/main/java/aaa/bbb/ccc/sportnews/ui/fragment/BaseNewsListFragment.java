ea593134d401380bd70fe8032d62724268862fe5
miha <vadim.hramov.69@mail.ru>
2019-01-06 21:33:21 +0200

    clean code

--- app/src/main/java/aaa/bbb/ccc/sportnews/ui/fragment/BaseNewsListFragment.java
+++
@@ -1,9 +0,0 @@
-package aaa.bbb.ccc.sportnews.ui.fragment;
-
-import com.arellomobile.mvp.MvpAppCompatFragment;
-
-import aaa.bbb.ccc.sportnews.mvp.model.pojo.NewsSource;
-
-public abstract class BaseNewsListFragment extends MvpAppCompatFragment {
-    abstract public void displayNews(NewsSource tag);
-}
